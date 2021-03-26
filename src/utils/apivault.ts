2f2ec86fcef373d62fa9c8cfd2da7cd87cdadf16
Deiver Carrascal <ingendeiver@gmail.com>
2021-03-26 15:10:47 -0500

    rejectUnauthorized apply

--- src/utils/apivault.ts
+++ src/utils/apivault.ts
@@ -1,13 +1,17 @@
 import axios, { AxiosResponse } from 'axios'
 import '../config/dotenv'
 const TOKEN = process.env.VAULT_TOKEN
+import https from 'https'
 
 export const queryVault = (uri: string) => {
   return new Promise((resolve, reject) => {
     const options = {
       headers:{
         'Authorization': `Bearer ${TOKEN}`
-      }
+      },
+      httpsAgent: new https.Agent({   
+        rejectUnauthorized: false
+      }),
     }
   
     axios.get(`${process.env.VAULT_HOST}${uri}`, options)
